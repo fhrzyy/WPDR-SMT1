29b6a277a2ce66a2af9bf62bd6a0c0d5d60559a6
ajax-lang <nexaskanamber@gmail.com>
2026-09-21 15:51:28 +0700

    inisal S cantik banget

--- Minggu 5/Tugas_4.java
+++
@@ -1,3 +0,0 @@
-public class Tugas_4 {
-    
-}
---
+++ Tugas_2.java
@@ -0,0 +1,23 @@
+public class Tugas_2 {
+    public static void main(String[] args) {
+        int bulan, year;
+        
+        bulan = 1;
+        year = 2002;
+        switch (bulan) {
+            case 1: 
+                switch (year) {
+                    case 2002:
+                        System.out.println("Bulan 1 tahun 2002");
+                        break;
+                        case 2003:
+                        System.out.println();
+                }
+            case 2:
+                default:
+                System.out.println("");
+                break;
+        }
+
+    }
+}
