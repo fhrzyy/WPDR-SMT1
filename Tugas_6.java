5dc99929555f65fef5644715586f45153e38eaf7
ajax-lang <nexaskanamber@gmail.com>
2026-09-22 05:23:51 +0700

    inisial S tolong notice aku pls

--- Tugas_6.java
+++ Tugas_6.java
@@ -1,3 +1,19 @@
 public class Tugas_6 {
-    main
+    public static void main(String[] args){
+
+    int expression = 5;
+    
+    switch(expression) {
+        case 2:
+            System.out.println("Small Size");
+        
+        case 3:
+            System.out.println("Large Size");
+
+        default:
+            System.out.println("Unknown Size");
+
+    }
+
+    }
 }
